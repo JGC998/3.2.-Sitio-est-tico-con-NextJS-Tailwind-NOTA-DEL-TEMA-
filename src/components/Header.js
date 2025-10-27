f07a901e681c00405558074f2c050e1344a09920
JGC998 <juangarciacardenas99@gmail.com>
2025-10-27 19:04:27 +0100

    Implementación de iconos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import ThemeSelect from "./ThemeSelect";
+import { FaRocket, FaClock, FaCalendarAlt, FaBalanceScale, FaCogs, FaHome, FaBars } from 'react-icons/fa';
 
 function Header() {
     return (
@@ -7,25 +8,43 @@ function Header() {
         <div className="navbar bg-base-500 shadow-sm">
 
             <div className="flex-1">
-                <Link href="/" className="btn btn-ghost text-xl">Inicio</Link>
+                <Link href="/" className="btn btn-ghost text-xl">
+                    <FaHome className="mr-2" />
+                    Inicio
+                </Link>
                 <ThemeSelect />
             </div>
             <div className="flex-none pr-20">
                 <ul className="menu menu-horizontal px-1">
                     <li>
-                        <Link href="/pagina1">Registro de lanzamiento</Link>
+                        <Link href="/pagina1">
+                            <FaRocket />
+                            Registro de lanzamiento
+                        </Link>
                     </li>
                     <li>
-                        <Link href="/pagina2">Reloj de Misión</Link>
+                        <Link href="/pagina2">
+                            <FaClock />
+                            Reloj de Misión
+                        </Link>
                     </li>
                     <li>
-                        <Link href="/pagina3">Planificador de Fechas de misión</Link>
+                        <Link href="/pagina3">
+                            <FaCalendarAlt />
+                            Planificador
+                        </Link>
                     </li>
                     <li>
-                        <Link href="/pagina4">Comparativa de Cohetes</Link>
+                        <Link href="/pagina4">
+                            <FaBalanceScale />
+                            Comparativa
+                        </Link>
                     </li>
                     <li>
-                        <Link href="/pagina5">Hardware de la misión</Link>
+                        <Link href="/pagina5">
+                            <FaCogs />
+                            Hardware
+                        </Link>
                     </li>
                     
 
@@ -37,17 +56,19 @@ function Header() {
                         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
                         <div className="drawer-content fixed top-0 right-0 z-10">
                             {/* Page content here */}
-                            <label htmlFor="my-drawer" className=" btn btn-primary drawer-button size-15 rounded-full">Navegación</label>
+                            <label htmlFor="my-drawer" className=" btn btn-primary drawer-button size-15 rounded-full">
+                                <FaBars />
+                            </label>
                         </div>
                         <div className="drawer-side z-10">
                             <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
                             <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                                 {/* Sidebar content here */}
-                                <li><Link href="/pagina1">Registro de lanzamiento</Link></li>
-                                <li><Link href="/pagina2">Reloj de Misión</Link></li>
-                                <li><Link href="/pagina3">Planificador de Fechas de misión</Link></li>
-                                <li><Link href="/pagina4">Comparativa de Cohetes</Link></li>
-                                <li><Link href="/pagina5">Hardware de la misión</Link></li>
+                                <li><Link href="/pagina1"><FaRocket className="mr-2" />Registro de lanzamiento</Link></li>
+                                <li><Link href="/pagina2"><FaClock className="mr-2" />Reloj de Misión</Link></li>
+                                <li><Link href="/pagina3"><FaCalendarAlt className="mr-2" />Planificador de Fechas</Link></li>
+                                <li><Link href="/pagina4"><FaBalanceScale className="mr-2" />Comparativa de Cohetes</Link></li>
+                                <li><Link href="/pagina5"><FaCogs className="mr-2" />Hardware de la misión</Link></li>
                             </ul>
                         </div>
                     </div>
